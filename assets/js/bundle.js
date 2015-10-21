5aa1cfb6a2d4b8a76a2d3cb90af11198294fd032
bodyjar <bodyjar@hotmail.com>
2015-10-21 00:12:47 -0400

    recommendation floating widget and always on top header

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -1,7 +1,16 @@
 document.getElementById("search").addEventListener("keyup", function(e){
     if(e.keyCode === 13){
         document.getElementById("container").setAttribute("class", "container focused");
-        document.getElementById("results").style.opacity="1";
+        document.getElementById("results").setAttribute("class", "show");
     }
         }
 );
+
+window.addEventListener("scroll", function(){
+    if (this.scrollY > 100) {
+        document.getElementById("header").setAttribute("class", "widget-desc scrolled");
+    }
+    else {
+        document.getElementById("header").setAttribute("class", "widget-desc");
+    }
+});
